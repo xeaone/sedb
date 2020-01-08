ed1fa0b9cd74e8a6f7e5de82baf689aa1aa0e90a
Alexander Elias <alex.steven.elias@gmail.com>
2020-01-08 14:16:46 -0800

    improved and bug fixed gsi

--- tst/db.js
+++ tst/db.js
@@ -3,7 +3,9 @@ const Sedb = require('../index.js');
 const Db = new Sedb.dynamo({
     name: 'test-sedb',
     region: 'us-west-2',
-	version: '2012-08-10'
+    version: '2012-08-10',
+    primary: { hash: 'uid' },
+    indexes: [ { hash: 'gid' } ]
 });
 
 module.exports = Db;
