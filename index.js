5bae331bd3d805b7e9200eb6fdb25412bbc56e11
Alexander Elias <alex.steven.elias@gmail.com>
2020-01-17 10:03:01 -0800

    0.12.1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-'use strict';
-
-const S3 = require('./lib/s3.js');
 const Dynamo = require('./lib/dynamo.js');
+const S3 = require('./lib/s3.js');
 
 module.exports = {
-	s3: S3,
-	dynamo: Dynamo
+    s3: S3,
+    dynamo: Dynamo
 };
