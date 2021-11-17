e0606f2467b145f64c45fe2334706435ab49aced
Alexander Elias <alex.steven.elias@gmail.com>
2021-11-16 16:36:43 -0800

    fixed spelling

--- tst/db.js
+++ tst/db.js
@@ -2,6 +2,7 @@ const Sedb = require('../index.js');
 
 const Db = new Sedb.dynamo({
     name: 'sedb',
+    profile: 'arc',
     region: 'us-west-2',
     version: '2012-08-10',
     primary: { hash: 'uid' },
